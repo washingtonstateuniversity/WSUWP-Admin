4ef6268b54efe4ec7b894e4adb08ccaaba26632e
jeremyfelt <jeremy.felt@gmail.com>
2014-11-14 14:05:28 -0800

    Bump version to 0.1.2

--- wsuwp-admin.php
+++ wsuwp-admin.php
@@ -4,7 +4,7 @@ Plugin Name: WSU Admin
 Plugin URI: http://web.wsu.edu
 Description: Customized portions of the admin area of WordPress for Washington State University
 Author: washingtonstateuniversity, jeremyfelt
-Version: 0.1.1
+Version: 0.1.2
 */
 
 class WSU_Admin {
