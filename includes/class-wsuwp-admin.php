8b88004e817273efe9ea3877cf88ba63a270d2a2
Jeremy Felt <jeremy.felt@gmail.com>
2017-11-07 13:40:31 -0800

    Move `post_password_required()` to a common location
    This was previously called as a method in the class and now
    causes a fatal error. :(

--- includes/class-wsuwp-admin.php
+++ includes/class-wsuwp-admin.php
@@ -352,39 +352,6 @@ class WSUWP_Admin {
 	}
 
 	/**
-	 * Check if the entry of a password is still required for a document.
-	 *
-	 * This is a duplicate of the WordPress function and is used only because
-	 * we don't have a proper `$post` object built with `post_password`
-	 * included when we need to do our check.
-	 *
-	 * @see WordPress post_password_required()
-	 *
-	 * @param string $post_password Password possibly assigned to a post.
-	 *
-	 * @return bool True if a password is required. False if not.
-	 */
-	private function post_password_required( $post_password ) {
-		if ( empty( $post_password ) ) {
-			return false;
-		}
-
-		if ( ! isset( $_COOKIE[ 'wp-postpass_' . COOKIEHASH ] ) ) {
-			return true;
-		}
-
-		require_once ABSPATH . WPINC . '/class-phpass.php';
-		$hasher = new PasswordHash( 8, true );
-
-		$hash = wp_unslash( $_COOKIE[ 'wp-postpass_' . COOKIEHASH ] );
-		if ( 0 !== strpos( $hash, '$P$B' ) ) {
-			return true;
-		}
-
-		return ! $hasher->CheckPassword( $post_password, $hash );
-	}
-
-	/**
 	 * Catch any checks for theme updates and remove our custom themes from the object. We don't
 	 * want to deal with false positives because of themes in the repository with the same slug.
 	 *
