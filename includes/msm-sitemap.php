b507c2375d906416d0344966ee825c5d2d886d99
Jeremy Felt <jeremy.felt@gmail.com>
2018-06-04 15:56:22 -0700

    Transfer a previous MSM filter from the main class

--- includes/msm-sitemap.php
+++ includes/msm-sitemap.php
@@ -3,6 +3,7 @@
 namespace WSUWP\Admin\MSM_Sitemap;
 
 add_filter( 'msm_sitemap_entry_post_type', __NAMESPACE__ . '\\supported_post_types' );
+add_action( 'init', __NAMESPACE__ . '\\remove_msm_sitemap_admin', 11 );
 
 /**
  * Filter the post types supported by the MSM Sitemap plugin.
@@ -18,4 +19,14 @@ function supported_post_types() {
 	);
 
 	return $post_types;
+}
+
+/**
+ * Hide the admin menu and screen for the MSM Sitemap plugin. The building of
+ * sitemaps through this plugin is controled solely through WP-CLI and cron.
+ *
+ * @since 1.4.3
+ */
+function remove_msm_sitemap_admin() {
+	remove_action( 'admin_menu', array( 'Metro_Sitemap', 'metro_sitemap_menu' ) );
 }
