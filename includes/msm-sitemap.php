1b1863adf0ed85dc693001b480769d791c11e35d
Jeremy Felt <jeremy.felt@gmail.com>
2018-06-04 15:55:56 -0700

    Actually add the filter for post type

--- includes/msm-sitemap.php
+++ includes/msm-sitemap.php
@@ -2,6 +2,8 @@
 
 namespace WSUWP\Admin\MSM_Sitemap;
 
+add_filter( 'msm_sitemap_entry_post_type', __NAMESPACE__ . '\\supported_post_types' );
+
 /**
  * Filter the post types supported by the MSM Sitemap plugin.
  *
